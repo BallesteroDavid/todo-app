dee7e94388adbf46e8e34ab5c35689af8392a44c
edouard-todorov-formation <edouard.todorov@laplateforme.io>
2025-12-09 11:55:04 +0100

    ajout_de_css_et_legere_modif_sur_front

--- frontend/src/components/WellcomePage.jsx
+++ frontend/src/components/WellcomePage.jsx
@@ -3,8 +3,16 @@ import { Link } from "react-router-dom";
 function WellcomePage() {
 
     return (
-        <div>
-            <h2 className="lienTasks"><Link to={'/tasks'}>Clique ici <br /> pour acceder a une todolist fantastique qu'on aimerait l'utiliser tout les jours comme une todolist</Link></h2>
+        <div className="min-h-screen flex items-center justify-center -mt-40 -mb-40">
+            <h2 className="text-center">
+                <Link 
+                    to={'/tasks'} 
+                    className="relative inline-block text-blue-600 hover:text-blue-800 transition-all duration-500 ease-in-out
+                               hover:scale-125 hover:rotate-3 transform">
+                    Clique ici <br /> 
+                    pour acceder a une todolist fantastique qu'on aimerait l'utiliser tout les jours comme todolist
+                </Link>
+            </h2>
         </div>
     )
 }
